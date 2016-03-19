d39d1a654224864b9846a2186808226bfb91ebea
roma2907 <ramageromain@gmail.com>
2016-03-19 15:20:12 +0100

    ajout du fragment de controle du drone

--- FireDrone/app/src/main/java/projet/istic/fr/firedrone/MainActivity.java
+++ FireDrone/app/src/main/java/projet/istic/fr/firedrone/MainActivity.java
@@ -1,7 +1,7 @@
 package projet.istic.fr.firedrone;
-
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -9,8 +9,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.o3dr.android.client.ControlTower;
+import com.o3dr.android.client.interfaces.TowerListener;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,TowerListener {
+
+    //tower pour se connecter au drone et recevoir les évènements du drone
+    private ControlTower controlTower;
+
+    //fragment du controle du drone
+    private DroneControlFragment fragmentControlDrone;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.controlTower.connect(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.controlTower.disconnect();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +40,11 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //initialisation de la tower
+        this.controlTower = new ControlTower(getApplicationContext());
+        fragmentControlDrone = new DroneControlFragment();
+        FragmentTransaction transaction  =  getSupportFragmentManager().beginTransaction().add(R.id.droneControle,fragmentControlDrone);
+        transaction.commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -67,5 +93,21 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public ControlTower getControlTower(){
+        return controlTower;
+    }
+
+    @Override
+    public void onTowerConnected() {
+        //on enregistre le drone dans la tower
+        controlTower.registerDrone(fragmentControlDrone.getDrone(), fragmentControlDrone.getHandler());
+        fragmentControlDrone.registerDroneListener();
+    }
+
+    @Override
+    public void onTowerDisconnected() {
+
     }
 }
