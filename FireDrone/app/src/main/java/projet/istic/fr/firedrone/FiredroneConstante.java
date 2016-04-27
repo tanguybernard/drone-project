fbea0df34fcfb19359e87fe2888c075505bfd06a
Tanguy Bernard <bernardtanguy1pro@gmail.com>
2016-04-27 23:26:28 +0200

    change amzon to istic

--- FireDrone/app/src/main/java/projet/istic/fr/firedrone/FiredroneConstante.java
+++ FireDrone/app/src/main/java/projet/istic/fr/firedrone/FiredroneConstante.java
@@ -6,7 +6,9 @@ import java.text.SimpleDateFormat;
  * Created by ramage on 22/04/16.
  */
 public class FiredroneConstante {
-    //public static final String END_POINT= "http://m2gla-drone.istic.univ-rennes1.fr:8080";
-    public static final String END_POINT= "http://ec2-52-31-150-254.eu-west-1.compute.amazonaws.com:8080";
+    public static final String END_POINT= "http://m2gla-drone.istic.univ-rennes1.fr:8080";
+    //public static final String END_POINT= "http://ec2-52-31-150-254.eu-west-1.compute.amazonaws.com:8080";
     public static final SimpleDateFormat DATE_FORMAT= new SimpleDateFormat("yyyyMMdd HHmm");
 }
+
+
