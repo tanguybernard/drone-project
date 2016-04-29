94ff3b829e004ae18138cfce37c3b45b3c5c6825
cRoptin <christophe.roptin@etudiant.univ-rennes1.fr>
2016-04-29 11:16:32 +0200

    Gestion des roles pour le tableau des moyens

--- FireDrone/app/src/main/java/projet/istic/fr/firedrone/FiredroneConstante.java
+++ FireDrone/app/src/main/java/projet/istic/fr/firedrone/FiredroneConstante.java
@@ -13,6 +13,10 @@ public class FiredroneConstante {
     //public static final String END_POINT= "http://ec2-52-31-150-254.eu-west-1.compute.amazonaws.com:8080";
     public static final SimpleDateFormat DATE_FORMAT= new SimpleDateFormat("yyyyMMdd HHmm");
 
+    public static final String ROLE_CODIS = "ROLE_CODIS";
+    public static final String ROLE_COS = "ROLE_COS";
+    public static final String ROLE_SIT = "ROLE_SIT";
+
     public static Toast getToastError(Context context){
         return Toast.makeText(context,"Impossible d'accéder au serveur",Toast.LENGTH_LONG);
     }
