780fe817bfc0853d238a9070aa61d691de2cbad5
roma2907 <ramageromain@gmail.com>
2016-05-26 17:39:06 +0200

    Paramètre lors de la création, segment en continu

--- projet-middle/python-script/create_drone.py
+++ projet-middle/python-script/create_drone.py
@@ -4,15 +4,17 @@ import dronekit_sitl
 import argparse
 parser = argparse.ArgumentParser(description='Print out vehicle state information. Connects to SITL on local PC by default.')
 parser.add_argument('--instance',default=0,help='Instance number')
+parser.add_argument('--longitude',default=0,help='Longitude')
+parser.add_argument('--lattitude',default=0,help='Latitude',)
+
 
 args = parser.parse_args()
 
 sitl = dronekit_sitl.SITL()
 sitl.download('copter', '3.3', verbose=True)
 sitl_args = ['-I0', '--model', 'quad','--instance',args.instance ]
-sitl_args.append('--home=1,1,584,353')
+sitl_args.append('--home='+ str(args.lattitude) + ','+ str(args.longitude) +',584,353')
 sitl.launch(sitl_args, await_ready=True, restart=True)
-sitl.block_until_ready(verbose=False) # explicitly wait until receiving commands
 code = sitl.complete(verbose=False) # wait until exit
 sitl.poll() # returns None or return code
 sitl.stop() # terminates SITL
