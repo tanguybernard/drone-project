a823a3123035c614fa009476816dfbfb7135c736
Tanguy Bernard <bernardtanguy1pro@gmail.com>
2016-05-30 12:56:29 +0200

    gallery images linked BDD

--- FireDrone/app/src/main/java/projet/istic/fr/firedrone/ImageMapFragment.java
+++ FireDrone/app/src/main/java/projet/istic/fr/firedrone/ImageMapFragment.java
@@ -60,6 +60,7 @@ public class ImageMapFragment extends SupportMapFragment implements
 
         //Si la liste est vide, on met les coordonnées de l'Ille-et-Vilaine
         if(listImage.isEmpty()) {
+            System.err.println("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
 
             final LatLng coordIlle = new LatLng(48.2292016, -1.5300694999999678);
             //Zoom sur la zone des marqueurs
@@ -72,10 +73,15 @@ public class ImageMapFragment extends SupportMapFragment implements
 
         }
         else {
+            System.err.println("YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+
             //Parcours de la liste des interventions
             for (int i = 0; i < listImage.size(); i++) {
                 //Si on a bien une latitude et une longitude, on met le marqueur
                 if ((listImage.get(i).getLatitude() != null) && (listImage.get(i).getLongitude() != null)) {
+
+                    System.err.println("YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS2");
+
                     //Cast de la latitude et de la longitude
                     try {
                         Double latitude = Double.parseDouble(listImage.get(i).getLatitude());
